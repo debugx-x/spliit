4070174ac25225e162f1f891ee4cabbe5d009cc7
debugx-x <vaibhavsaini2013@gmail.com>
2026-05-15 22:58:08 -0700

    chore: migrate database from SQLite to PostgreSQL and update deployment scripts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,8 +7,7 @@ const interpretEnvVarAsBool = (val: unknown): boolean => {
 
 const envSchema = z
   .object({
-    POSTGRES_URL_NON_POOLING: z.string().url(),
-    POSTGRES_PRISMA_URL: z.string().url(),
+    POSTGRES_URL: z.string().url(),
     NEXT_PUBLIC_BASE_URL: z
       .string()
       .optional()
